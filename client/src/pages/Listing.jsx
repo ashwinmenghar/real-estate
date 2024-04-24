bfd34afd3f76caa2ef7a0a362570bb8601e01e6e
Ashwin Menghar <ashwinmenghar123@gmail.com>
2024-04-24 16:36:19 +0530

    add space

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -49,7 +49,7 @@ export default function Listing() {
   }, [params.listingId]);
 
   return (
-    <main className="h-full">
+    <main className="h-full ">
       {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
       {error && (
         <p className="text-center my-7 text-2xl">Something went wrong!</p>
